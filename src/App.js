9d6041d85e80253cb0a6fc8f247260c932ba7e3b
“João <eng.joao.pedro@outlook.com>
2023-02-05 20:56:20 -0300

    refactor: fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+const clientId = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/;`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoadin] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState([]);
+  const [query, setQuery] = useState([]);
+  
+
+  const fetchImages = async () => {
+    setLoadin(true);
+    let url
+    url = `${mainUrl}${clientId}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setLoadin(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  return <div>Stock</div>;
 }
 
 export default App;
